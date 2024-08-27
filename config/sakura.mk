3cad37d1df444b6d5b09808dbf7614a3fab7762f
Pranav Vashi <neobuddy89@gmail.com>
2024-08-27 22:29:08 +0530

    Sakura: Always disable app launch blur and add vendor blur support [SQUASHED]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/sakura.mk
+++ config/sakura.mk
@@ -6,6 +6,16 @@ PRODUCT_PACKAGES += \
     GameSpace \
     OmniJaws
 
+# Blurs
+ifeq ($(TARGET_ENABLE_BLUR), true)
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
+endif
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.launcher.blur.appLaunch=0
+
 ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
 PRODUCT_PACKAGES += \
     FaceUnlock
